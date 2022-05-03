8db4590de269ff56ed77fe3219126a97eea0d9f2
sambuite <murilosambuite@gmail.com>
2022-05-03 12:16:39 -0300

    add WidgetForm Steps; add ScreenshotButton; add Loading; add CloseButton

---
+++ src/components/WidgetForm/index.tsx
@@ -0,0 +1,77 @@
+import { useState } from 'react';
+import bugImageUrl from '../../assets/bug.svg';
+import ideaImageUrl from '../../assets/idea.svg';
+import thoughtImageUrl from '../../assets/thought.svg';
+import { FeedbackContentStep } from './Steps/FeedbackContentStep';
+import { FeedbackSuccessStep } from './Steps/FeedbackSuccessStep';
+import { FeedbackTypeStep } from './Steps/FeedbackTypeStep';
+
+export const feedbackTypes = {
+  BUG: {
+    title: 'Problema',
+    image: {
+      source: bugImageUrl,
+      alt: 'Image de um inseto',
+    },
+  },
+  IDEA: {
+    title: 'Idea',
+    image: {
+      source: ideaImageUrl,
+      alt: 'Image de uma lâmpada',
+    },
+  },
+  OTHER: {
+    title: 'Problema',
+    image: {
+      source: thoughtImageUrl,
+      alt: 'Image de uma nuvem de pensamento',
+    },
+  },
+};
+
+export type FeedbackType = keyof typeof feedbackTypes;
+
+export function WidgetForm() {
+  const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null);
+  const [feedbackSent, setFeedbackSent] = useState(false);
+
+  function handleRestartFeedback() {
+    setFeedbackSent(false);
+    setFeedbackType(null);
+  }
+
+  return (
+    <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
+      {feedbackSent ? (
+        <FeedbackSuccessStep
+          onFeedbackRestartRequested={handleRestartFeedback}
+        />
+      ) : (
+        <>
+          {!feedbackType ? (
+            <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType} />
+          ) : (
+            <FeedbackContentStep
+              feedbackType={feedbackType}
+              onFeedbackRestartRequested={handleRestartFeedback}
+              onFeedbackSent={() => setFeedbackSent(true)}
+            />
+          )}
+        </>
+      )}
+
+      <footer className="text-xs text-neutral-400">
+        <p>
+          Feito com ♥ por{' '}
+          <a
+            className="underline underline-offset-2"
+            href="https://www.linkedin.com/in/murilosambuite/"
+          >
+            Murilo
+          </a>
+        </p>
+      </footer>
+    </div>
+  );
+}
